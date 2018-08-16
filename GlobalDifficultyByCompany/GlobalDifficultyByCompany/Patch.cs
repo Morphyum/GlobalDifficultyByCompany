a7765e7c5962440088d53808090630210d55cdc9
Morphyum <zion@c-schwabe.de>
2018-08-16 18:12:32 +0200

    0.1.3

--- GlobalDifficultyByCompany/GlobalDifficultyByCompany/Patch.cs
+++ GlobalDifficultyByCompany/GlobalDifficultyByCompany/Patch.cs
@@ -61,4 +61,19 @@ namespace GlobalDifficultyByCompany {
             }
         }
     }
+
+    [HarmonyPatch(typeof(SimGameState), "ShowDifficultMissionPopup")]
+    public static class SimGameState_ShowDifficultMissionPopup {
+
+        static bool Prefix(SimGameState __instance, SimGameInterruptManager ___interruptQueue) {
+            try {
+                ___interruptQueue.QueuePauseNotification("Difficult Mission", "Careful, Commander. This drop looks like it might require more firepower than that.", __instance.GetCrewPortrait(SimGameCrew.Crew_Darius), string.Empty, new Action(__instance.RoomManager.CmdCenterRoom.lanceConfigBG.LC.ContinueConfirmClicked), "CONFIRM", null, "BACK");
+                return false;
+            }
+            catch (Exception e) {
+                Logger.LogError(e);
+                return true;
+            }
+        }
+    }
 }
